9219f80eeaf3fca65a042d7f272d0a2b6b9c8147
Alleluia Izimpamvu <58779456+aizimpamvu@users.noreply.github.com>
2022-03-21 20:28:49 +0000

    https://opentdb.com Open trivia db. For dynamic questions

--- data.py
+++ data.py
@@ -1,14 +1,12 @@
-question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+question_data =[
+                {
+                   "category":"Science: Computers",                    
+                   "type":"boolean","difficulty":"medium",
+                   "question":"The HTML5 standard was published in 2014.",
+                   "correct_answer":"True",
+                   "incorrect_answers":["False"]
+                },
+                {
+                  "category":"Science: Computers","type":"boolean","difficulty":"medium","question":"FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;","correct_answer":"False","incorrect_answers":["True"]},
+    {"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"The logo for Snapchat is a Bell.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"Ada Lovelace is often considered the first computer programmer.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"The open source program Redis is a relational database server.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Computers","type":"boolean","difficulty":"hard","question":"DHCP stands for Dynamic Host Configuration Port.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"Early RAM was directly seated onto the motherboard and could not be easily removed.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"boolean","difficulty":"medium","question":"The first dual-core CPU was the Intel Pentium D.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Computers","type":"boolean","difficulty":"hard","question":"The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.","correct_answer":"True","incorrect_answers":["False"]}
+  ]
