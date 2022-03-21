ab1d8a59b408fd1baa9b35cdd338694e96b576a9
Alleluia Izimpamvu <58779456+aizimpamvu@users.noreply.github.com>
2022-03-21 20:01:40 +0000

    check answer

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
   def __init__(self,question_list):
     self.question_list=question_list
     self.question_number=0
+    self.score=0
   def still_has_question(self):
     return self.question_number< len(self.question_list)
     
@@ -14,9 +15,12 @@ class QuizBrain:
   def check_answer(self,user_answer,correct_answer):
     if user_answer.lower()==correct_answer.lower():
       print("You got it right")
+      self.score +=1
     else:
       print("That's wrong")
-      print(f"The correct answer was {correct_answer}")
+    print(f"The correct answer was {correct_answer}")
+    print(f" Your score is {self.score}/{self.question_number}")
+    print("\n")
       
 
   
